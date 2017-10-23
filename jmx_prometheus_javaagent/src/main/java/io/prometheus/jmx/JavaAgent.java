dfc916d18a119ee122fb1e914135f9a384ffb0b8
gsvic <victorasgs@gmail.com>
2017-10-23 12:03:40 +0300

    Minor change to usage message

--- jmx_prometheus_javaagent/src/main/java/io/prometheus/jmx/JavaAgent.java
+++ jmx_prometheus_javaagent/src/main/java/io/prometheus/jmx/JavaAgent.java
@@ -3,7 +3,10 @@ package io.prometheus.jmx;
 import java.io.*;
 import java.lang.instrument.Instrumentation;
 import java.net.*;
+import java.util.Arrays;
+import java.util.List;
 
+import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
 import io.prometheus.client.hotspot.DefaultExports;
@@ -16,7 +19,7 @@ public class JavaAgent {
    public static void premain(String agentArgument, Instrumentation instrumentation) throws Exception {
      String[] args = agentArgument.split(":");
      if (args.length < 2 || args.length > 4) {
-       System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file>:<exporter property file>");
+       System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file>:<exporter .property file>");
        System.exit(1);
      }
 
