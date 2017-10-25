0038f69f9e0f7c8c3a04af103c37dea1e71f3687
gsvic <victorasgs@gmail.com>
2017-10-25 17:43:36 +0300

    feature-spark-tweaks changed to feature_spark_tweaks

--- make-jars.sh
+++ make-jars.sh
@@ -6,12 +6,11 @@ CLIENT_HOTSPOT_DIR=$JMX_EXPORTER_DIR/client_java/simpleclient_hotspot
 echo "simpleclient_hotspot directory set to: $CLIENT_HOTSPOT_DIR"
 echo "jmx_exporter directory set to: $JMX_EXPORTER_DIR" 
 
-git checkout feature-spark-tweaks 
-git submodule update --recursive --remote
+git submodule update --init
 
 echo "Building BEAT simpleclient_hotspot project..."
 cd $CLIENT_HOTSPOT_DIR
-git checkout feature-spark-tweaks
+git checkout feature_spark_tweaks
 mvn clean install -DskipTests
 mvn install:install-file -Dfile=target/simpleclient_hotspot-beat.jar -DgroupId=io.prometheus -DartifactId=simpleclient_hotspot_beat -Dversion=0.0.26 -Dpackaging=jar
 
